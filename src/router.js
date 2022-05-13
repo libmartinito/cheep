d8b45463204571d76b0cd42619ae0aaa07c66d91
libmartinito <lib.martinito@cambridge.org>
2022-05-13 12:14:48 +0800

    Added routes

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CheepHome from './pages/CheepHome.vue'
+import CheepExplore from './pages/CheepExplore.vue'
+import CheepNotifications from './pages/CheepNotifications.vue'
+import CheepMessages from './pages/CheepMessages.vue'
+import CheepProfile from './pages/CheepProfile.vue'
+
+const routes = [
+    { path: '/', component: CheepHome },
+    { path: '/explore', component: CheepExplore },
+    { path: '/notifications', component: CheepNotifications },
+    { path: '/messages', component: CheepMessages },
+    { path: '/profile', component: CheepProfile },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
